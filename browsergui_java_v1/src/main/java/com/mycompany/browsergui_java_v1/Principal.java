e6fc600ae55afb86365a04b655f2907916b62372
Jorge <Jorge@LAPTOP-13O2AL1N>
2024-04-06 10:29:27 -0300

    projeto navegador com socket

---
+++ browsergui_java_v1/src/main/java/com/mycompany/browsergui_java_v1/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.browsergui_java_v1;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmBrowser janela = new FrmBrowser();
+        janela.setVisible(true);
+    }
+}
